de3533fe81ef41f11b0d8cc9191ddf9af74c1993
maxjgonzaga <maxjgonzaga@gmail.com>
2024-02-27 16:53:06 -0300

    prep para render

--- projeto_investa_me/settings.py
+++ projeto_investa_me/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import os
+# import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'v#xjpat=*nkh^umn$rxb%qly55nfy)75*4r!i75%(zcqyl*kgk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
